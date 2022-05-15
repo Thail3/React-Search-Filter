298e64be00d0ace6f5754c6ca36b109d0f0a88e9
Thail3 <tanai.saeng@gmail.com>
2022-05-15 15:43:20 +0700

    multi search filter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import { useState } from "react";
 import "./App.css";
+import Table from "./Table";
 import { Users } from "./users";
 
 function App() {
   const [query, setQuery] = useState("");
   console.log(query);
+
+  const keys = ["first_name", "last_name", "email"];
+
+  console.log(Users[0]["email"]);
+
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query))
+    );
+  };
+
+  // const search = (data) => {
+  //   return data.filter(
+  //     (item) =>
+  //       item.first_name.toLowerCase().includes(query) ||
+  //       item.last_name.toLowerCase().includes(query) ||
+  //       item.email.toLowerCase().includes(query)
+  //   );
+  // };
 
   return (
     <div className="App">
@@ -14,17 +34,7 @@ function App() {
         className="search"
         onChange={(e) => setQuery(e.target.value)}
       />
-      <ul className="list">
-        {Users.filter((user) =>
-          user.first_name.toLowerCase().includes(query)
-        ).map((user) => {
-          return (
-            <li key={user.id} className="listItem">
-              {user.first_name}
-            </li>
-          );
-        })}
-      </ul>
+      <Table data={search(Users)} />
     </div>
   );
 }
